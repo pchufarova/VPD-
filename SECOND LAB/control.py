4a0e0cd0ce2570a89c333468025734ad25193406
Polina <chufarovap05@mail.ru>
2024-02-21 12:15:59 +0300

    control для 2 лабы

--- SECOND LAB/control.py
+++ SECOND LAB/control.py
@@ -6,16 +6,13 @@ voltages = [10, 15, 20, 25, 30, 35, 40, 45, 50, -50, -45, -40, -35, -30, -25, -2
 motor_a = motor.LargeMotor(motor.OUTPUT_B)
 for vol in voltages:
     startTime = time.time()
-    # f = open('.csv'.format(vol),'w')
     while (True):
         currentTime = time.time() - startTime
         # motor_pose = motor_a.position
         # motor_vel = motor_a.speed
         motor_a.run_direct(duty_cycle_sp = vol)
-        # f.write("{} {} {}\n".format(currentTime, motor_pose, motor_vel))
 
-        if currentTime > 1:
+        if currentTime > 3:
             motor_a.run_direct(duty_cycle_sp = 0)
-            time.sleep(1)
+            time.sleep(10)
             break
-    # f.close()
