21460e2dcaf78d2079b8a038e72424d25168c396
Polina <chufarovap05@mail.ru>
2024-02-27 21:16:11 +0300

    правки

--- FIRST LAB/ГРАФИКИ МАТ. МОДЕЛЬ/math_model.m
+++ FIRST LAB/ГРАФИКИ МАТ. МОДЕЛЬ/math_model.m
@@ -1,6 +1,6 @@
 volts = [-100, -80, -60, -40, -20, 20, 40, 60, 80, 100];
 move = [19.5128, 31.8348, 40.9803, 46.9319, 49.7768, 46.8097, 40.352, 30.4211, 17.0693, 0];
-%volts = [80];
+%volts = [100];
 i=1;
 J = 0.0023;
 k = 0.05;
@@ -15,16 +15,16 @@ for vol = volts
     angle = angle*pi/180 - move(i);
     time = data(:,1);
     omega = data(:,3)*pi/180;
-    figure(i);
-    plot(time, angle);
-    hold on;
-    xlabel("time");
-    ylabel("angle");
     %figure(i);
-    %plot(time, omega);
+    %plot(time, angle);
     %hold on;
     %xlabel("time");
-    %ylabel("speed");
+    %ylabel("angle");
+    figure(i);
+    plot(time, omega);
+    hold on;
+    xlabel("time");
+    ylabel("speed");
 
     par0=[0.5, 1];
     fun = @(par,time)U_pr*par(1)*(time - par(2)*(1 - exp(-time/par(2))));
@@ -35,19 +35,21 @@ for vol = volts
     disp(k);
     disp(Tm);
     time_apr = 0:0.01:1;
-    theta = U_pr*k*(time_apr - Tm*(1 - exp(-time_apr/Tm)));
-    %omega_F = U_pr*k*(1-exp(-(time_apr/Tm)));
+    %theta = U_pr*k*(time_apr - Tm*(1 - exp(-time_apr/Tm)));
+    omega_F = U_pr*k*(1-exp(-(time_apr/Tm)));
     figure(i);
-    plot(time_apr, theta);
-    %plot(time_apr, omega_F);
+    %plot(time_apr, theta);
+    plot(time_apr, omega_F);
 
     simulink_data = sim("smth");
-    x = simulink_data.theta.Time;
-    y = simulink_data.theta.Data;
+    x = simulink_data.omega.Time;
+    y = simulink_data.omega.Data;
     figure(i);
     plot(x, y, '--');
-    xlabel("time, s");
-    ylabel("angle, rad");
+    xlabel("t, s");
+    ylabel("\omega, rad/s");
     hold off;
+    grid on;
+    legend("эксперимент", "аппроксимация", "модель")
     i=i+1;
 end
