702610117c76631c0c1735ccb4ec050be53ec686
Polina <chufarovap05@mail.ru>
2024-03-13 14:03:30 +0300

    исправление

---
+++ THIRD LAB/control_p_1.py
@@ -0,0 +1,32 @@
+import math
+import ev3dev2.motor as motor
+import time
+
+motor_a = motor.LargeMotor(motor.OUTPUT_B)
+angle_need = 90
+kp = [0.5, 1, 1.5, 2, 2.5, 3]
+
+for i in range(len(kp)):
+    startTime = time.time()
+    start_pose = motor_a.position
+    f = open('data_p_{}.csv'.format(i), 'w')
+    while (True):
+        currentTime = time.time() - startTime
+        pose = motor_a.position - start_pose
+
+        U = kp[i] * (angle_need - pose)
+
+        if U > 100:
+            motor_a.run_direct(duty_cycle_sp=100)
+        elif U < -100:
+            motor_a.run_direct(duty_cycle_sp=-100)
+        else:
+            motor_a.run_direct(duty_cycle_sp=U)
+
+        f.write("{} {}\n".format(currentTime, pose))
+
+        if currentTime > 3:
+            motor_a.run_direct(duty_cycle_sp=0)
+            time.sleep(1)
+            break
+f.close()
--- THIRD LAB/control_rele_1.py
+++ THIRD LAB/control_rele_1.py
@@ -3,7 +3,7 @@ import ev3dev2.motor as motor
 import time
 
 motor_a = motor.LargeMotor(motor.OUTPUT_B)
-angle_need = 90
+angle_need = 180
 
 U = 100
 
@@ -14,6 +14,7 @@ start_pose = motor_a.position
 while (True):
     currentTime = time.time() - startTime
     pose = motor_a.position - start_pose
+    f.write("{} {}\n".format(currentTime, pose))
 
     if pose < angle_need:
         motor_a.run_direct(duty_cycle_sp=U)
@@ -22,9 +23,7 @@ while (True):
     else:
         motor_a.run_direct(duty_cycle_sp=0)
 
-    f.write("{} {}\n".format(currentTime, pose))
-
-    if currentTime > 1:
+    if currentTime > 5:
         motor_a.run_direct(duty_cycle_sp=0)
         time.sleep(1)
         break
