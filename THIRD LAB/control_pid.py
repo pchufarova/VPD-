eaf3f7162fb918f2bdc06c57bd1d34c229652d6a
Polina <chufarovap05@mail.ru>
2024-03-13 15:01:53 +0300

    исправление

---
+++ THIRD LAB/control_pid.py
@@ -0,0 +1,32 @@
+import math
+import ev3dev2.motor as motor
+import time
+
+motor_a = motor.LargeMotor(motor.OUTPUT_B)
+angle_need = 180
+kp = [1, 3, 6]
+kd = [0, 1, 3]
+ki = [0, 3]
+integr_summ = 0
+for p in range(len(kp)):
+    for d in range(len(kd)):
+        for i in range(len(ki)):
+            startTime = time.time()
+            start_pose = motor_a.position
+            f = open('data_pid_{}_{}_{}.csv'.format(kp, kd, ki), 'w')
+            e = angle_need
+            while (True):
+                currentTime = time.time() - startTime
+                pose = motor_a.position - start_pose
+                f.write("{} {}\n".format(currentTime, pose))
+                e_p = e
+                e = angle_need - pose
+                integr_summ += e
+                U = kp[p]*e + ki[i]*integr_summ + kd[d]*(e-e_p)
+
+                if currentTime > 2:
+                    motor_a.run_direct(duty_cycle_sp=0)
+                    time.sleep(0.5)
+                    break
+
+f.close()
--- THIRD LAB/data2_p/graf_p_p.m
+++ THIRD LAB/data2_p/graf_p_p.m
@@ -1,11 +1,11 @@
-nums = [0, 1, 2, 3, 4, 5, 6, 7];
+nums = [05, 1, 15, 2, 25, 3, 35, 4, 45, 5, 55, 6, 65, 7, 75, 8, 9, 95];
 for n = nums
 
-    data = readmatrix(strcat("data_p_", num2str(n), ".csv"));
+    data = readmatrix(strcat("data_p_0_", num2str(n), ".csv"));
     angle = data(:,2);
     angle = angle*pi/180; 
     time = data(:,1)/1000;
-    figure(n+1);
+    figure(n);
     plot(time, angle);
     hold on;
     grid on;
