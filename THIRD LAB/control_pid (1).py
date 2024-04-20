da1c4f4e71fb2bc3ebaa7c7aaf01eb5659e6bf71
tameem bourgi <bourgitameem429@gmail.com>
2024-04-20 17:06:42 +0300

    Add files via upload

---
+++ THIRD LAB/control_pid (1).py
@@ -0,0 +1,66 @@
+# import math
+# import ev3dev2.motor as motor
+# import time
+
+# motor_a = motor.LargeMotor(motor.OUTPUT_B)
+# angle_need = 180
+# kp = [1, 3, 6]
+# kd = [0, 1, 3]
+# ki = [0, 3]
+# integr_summ = 0
+# for p in range(len(kp)):
+#     for d in range(len(kd)):
+#         for i in range(len(ki)):
+#             startTime = time.time()
+#             start_pose = motor_a.position
+#             f = open('data_pid_{}_{}_{}.csv'.format(kp, kd, ki), 'w')
+#             e = angle_need
+#             while (True):
+#                 currentTime = time.time() - startTime
+#                 pose = motor_a.position - start_pose
+#                 f.write("{} {}\n".format(currentTime, pose))
+#                 e_p = e
+#                 e = angle_need - pose
+#                 integr_summ += e
+#                 U = kp[p]*e + ki[i]*integr_summ + kd[d]*(e-e_p)
+#                 motor_a.run_direct(duty_cycle_sp=U)
+
+#                 if currentTime > 2:
+#                     motor_a.run_direct(duty_cycle_sp=0)
+#                     time.sleep(0.5)
+#                     break
+
+# f.close()
+import math
+import ev3dev2.motor as motor
+import time
+
+motor_a = motor.LargeMotor(motor.OUTPUT_B)
+angle_need = 180
+kp = [1, 3, 6]
+kd = [0, 1, 3]
+ki = [0, 3]
+
+for p in range(len(kp)):
+    for d in range(len(kd)):
+        for i in range(len(ki)):
+            startTime = time.time()
+            start_pose = motor_a.position
+            integr_summ = 0  # Reset integral sum for each set of parameters
+            with open('data_pid_{}_{}_{}.csv'.format(kp[p], kd[d], ki[i]), 'w') as f:
+                e = angle_need
+                while True:
+                    currentTime = time.time() - startTime
+                    pose = motor_a.position - start_pose
+                    f.write("{} {}\n".format(currentTime, pose))
+                    e_previous = e
+                    e = angle_need - pose
+                    integr_summ += e
+                    U = kp[p]*e + ki[i]*integr_summ + kd[d]*(e-e_previous)
+                    U = max(min(U, 100), -100)  # Assuming duty cycle range of -100 to 100
+                    motor_a.run_direct(duty_cycle_sp=U)
+
+                    if currentTime > 2:
+                        motor_a.run_direct(duty_cycle_sp=0)
+                        time.sleep(0.5)
+                        break
